624d8b8c0e26565a8596941dc2242abe5dbc8c76
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-08-19 15:25:14 -0400

    aws initial

--- src/try_dajngo/settings.py
+++ src/try_dajngo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #third party
     'crispy_forms',
     'rest_framework',
+    'storages',
 
     #apps
     'blog',
@@ -135,3 +136,15 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+AWS_ACCESS_KEY_ID = os.environ.get ('AWS_ACCESS_KEY_ID_TRY_DJANGO')
+AWS_SECRET_ACCESS_KEY = os.environ.get ('AWS_SECRET_ACCESS_KEY_TRY_DJANGO')
+AWS_STORAGE_BUCKET_NAME = os.environ.get ('AWS_STORAGE_BUCKET_NAME_TRY_DJANGO')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-2'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
