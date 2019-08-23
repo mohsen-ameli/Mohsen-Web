a68d83723824addee284e02549fb20c5dcaf1d73
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-08-22 23:00:07 -0400

    test

--- try_dajngo/settings.py
+++ try_dajngo/settings.py
@@ -138,8 +138,7 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL = "home"
 
-#STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
