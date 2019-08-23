2eb40609c286e508fc48796695f3b014187442af
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-08-23 16:17:46 -0400

    mobile displat

--- try_dajngo/settings.py
+++ try_dajngo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY_TRY_DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get("DEBUG_VALUE") == "False")
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ['mohsenweb.herokuapp.com', 'localhost']
 
