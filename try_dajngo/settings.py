2157953321df2c752daa5e10c23fc2a4042efc5b
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-08-23 15:34:38 -0400

    debug == false

--- try_dajngo/settings.py
+++ try_dajngo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY_TRY_DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == "False")
 
 ALLOWED_HOSTS = ['mohsenweb.herokuapp.com']
 
